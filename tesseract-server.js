4571ea89c0a15477a18a35c7032227e8ddce818a
brenor2 <rbrenorios@gmail.com>
2018-12-17 18:05:30 -0300

    trying to solve spawn error3

--- tesseract-server.js
+++ tesseract-server.js
@@ -79,7 +79,7 @@ function drugCheck(drug, callback) {
  }
 });
 
-var server = app.listen(8080, function() {
+var server = app.listen(process.env.PORT || port, function() {
 
 	var host = server.address().address;
 	var port = server.address().port;
@@ -87,4 +87,13 @@ var server = app.listen(8080, function() {
 	console.log("Example app listening at http://%s:%s", host, port);
 });
 
-server.listen(process.env.PORT || port);
+// var server = app.listen(8080, function() {
+
+// 	var host = server.address().address;
+// 	var port = server.address().port;
+
+// 	console.log("Example app listening at http://%s:%s", host, port);
+// });
+
+
+// server.listen(process.env.PORT || port);
